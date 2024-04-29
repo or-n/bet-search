896001c81c55e27859e0b9466580a9f8b5a1c173
or-n <kulombowie@gmail.com>
2024-04-29 23:45:18 +0200

    superbet odds extraction partial

--- src/superbet.rs
+++ src/superbet.rs
@@ -1,4 +1,5 @@
 use crate::bookmaker;
+use scraper::{Html, Selector};
 
 pub struct Book;
 
@@ -11,4 +12,37 @@ impl bookmaker::Site for Book {
 
     const COOKIE_ACCEPT_CSS: &'static str =
         r#"button[id="onetrust-accept-btn-handler"]"#;
+}
+
+impl bookmaker::GetOdds for Book {
+    fn get_odds(
+        site: &String,
+    ) -> Result<Vec<(bookmaker::Teams, bookmaker::Odds)>, ()> {
+        let document = Html::parse_document(site);
+        let match_selector = Selector::parse("div.event-card").unwrap();
+        let odds_selector =
+            Selector::parse("span.odd-button__odd-value-new").unwrap();
+        let team1_selector =
+            Selector::parse("div.e2e-event-team1-name").unwrap();
+        let team2_selector =
+            Selector::parse("div.e2e-event-team2-name").unwrap();
+        let team = |x: scraper::ElementRef| x.inner_html().trim().to_string();
+        let matches: Vec<_> = document
+            .select(&match_selector)
+            .map(|x| {
+                let team1 = x.select(&team1_selector).next().unwrap();
+                let team2 = x.select(&team2_selector).next().unwrap();
+                let teams = bookmaker::Teams {
+                    team1: team(team1),
+                    team2: team(team2),
+                };
+                let odds = x
+                    .select(&odds_selector)
+                    .map(|x| x.inner_html().trim().parse::<f32>().unwrap())
+                    .collect();
+                (teams, odds)
+            })
+            .collect();
+        Ok(matches)
+    }
 }
