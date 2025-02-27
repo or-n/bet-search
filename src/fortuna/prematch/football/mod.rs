fed495d25894abc1d05e318456a420cca5cca2f7
or-n <kulombowie@gmail.com>
2025-02-27 20:30:07 +0100

    football matches page scroll

--- src/fortuna/prematch/football/mod.rs
+++ src/fortuna/prematch/football/mod.rs
@@ -7,22 +7,39 @@ use crate::utils::{
     page::{Name, Tag},
 };
 use eat::*;
+use fantoccini::{error::CmdError, Client, Locator};
+use tokio::time::{sleep, Duration};
 
 const URL: &str = "/zaklady-bukmacherskie/pilka-nozna";
 
 #[derive(Clone, Copy)]
 pub struct Page;
 
-impl Download<fantoccini::Client, Page> for Tag<Page, String> {
-    type Error = fantoccini::error::CmdError;
+impl Download<Client, Page> for Tag<Page, String> {
+    type Error = CmdError;
 
     async fn download(
-        client: &mut fantoccini::Client,
+        client: &mut Client,
         _data: Page,
     ) -> Result<Self, Self::Error> {
         let url = format!("{}{}", super::URL, URL);
         client.goto(url.as_str()).await?;
         browser::try_accepting_cookie(client, COOKIE_ACCEPT).await?;
+        let mut previous_count = 0;
+        let mut new_count = 0;
+        let scroll = "window.scrollTo(0, document.body.scrollHeight);";
+        loop {
+            let elements = client.find_all(Locator::Css(".event-link")).await?;
+            new_count = elements.len();
+            if new_count == previous_count {
+                println!("nothing new");
+                break;
+            }
+            println!("found: {}", new_count);
+            previous_count = new_count;
+            client.execute(scroll, vec![]).await?;
+            sleep(Duration::from_secs(2)).await;
+        }
         client.source().await.map(Tag::new)
     }
 }
