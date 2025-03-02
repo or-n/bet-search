4d14bdde1648f9c9cee8abae40f1ec481d2a4442
or-n <kulombowie@gmail.com>
2025-03-02 09:17:00 +0100

    bmbets find search input

--- src/bin/bmbets.rs
+++ src/bin/bmbets.rs
@@ -1,10 +1,21 @@
+use fantoccini::{error::CmdError, Client, Locator};
 use odds::utils::browser;
 use std::time::Instant;
+
+const URL: &str = "https://www.bmbets.com/value-bets";
+
+async fn find_match(client: &mut Client) -> Result<(), CmdError> {
+    client.goto(URL).await?;
+    let search_input = client.find(Locator::Css("input.search")).await?;
+    search_input.send_keys("Hello\n").await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
     let start = Instant::now();
-    let client = browser::connect(4444).await;
+    let mut client = browser::connect(4444).await;
+    let _ = find_match(&mut client).await;
     client.close().await.unwrap();
     println!("Elapsed time: {:.2?}", start.elapsed());
 }
